07d7a5e00bd61151b50954554cae43b117bd5faf
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-07-10 11:26:38 -0300

    estudando exceptions

---
+++ src/main/java/com/daniel/javacore/exceptions/StarkOverflow01.java
@@ -0,0 +1,10 @@
+package com.daniel.javacore.exceptions;
+
+public class StarkOverflow01 {
+    public static void main(String[] args) {
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+}
